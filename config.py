0eab1b613e3508d3a33f6f6d65198392f470a42b
Chandler Swift <chandler@chandlerswift.com>
2019-08-07 18:48:52 -0500

    Add sample config file

---
+++ config.py
@@ -0,0 +1,11 @@
+# This file is included in the application config for both the UI and workers.
+
+# This is the executable that is run to test the application
+SCORING_BIN_NAME = "echo"
+
+# This is the name of the gold executable
+SCORING_GOLD_NAME = "gold"
+
+# This is the email/password of the admin user
+ADMIN_USER_EMAIL = "swift106@d.umn.edu"
+ADMIN_USER_PASSWORD = "password"
