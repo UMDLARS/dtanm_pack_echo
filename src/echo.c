115a504897db4f77cb6e889b1b64419b01ed1a2c
Chandler Swift <chandler@chandlerswift.com>
2019-10-01 23:16:34 +0000

    Remove trailing space from broken version

--- src/echo.c
+++ src/echo.c
@@ -9,8 +9,10 @@ int main (int argc, char** argv) {
 	for (int i = 1; i < argc; i++) {
 		strcpy(ptr, argv[i]); // Copy the argument to the buffer
 		ptr += strlen(argv[i]); // Increment through the buffer
-		*ptr = ' '; // Add a space between arguments
-		ptr++;
+		if (i < argc - 1) { // not the last argument -- dont't want a trailing space!
+			*ptr = ' '; // Add a space between arguments
+			ptr++;
+		}
 	}
 	printf("%s\n", buffer); // print out all the arguments
 	return 0;
